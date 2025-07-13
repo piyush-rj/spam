fb85b717979e552be2602a8f74859928c88807c2
Turbobot <turbobot@vercel.com>
2025-07-13 20:33:25 +0530

    feat(create-turbo): apply official-starter transform

---
+++ dapp/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="text-xl text-red-400">
+        ha bhai
+      </div>
+    </div>
+  );
+}
+
+export default App;

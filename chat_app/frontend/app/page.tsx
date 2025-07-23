ad19438c520d3a0972fffc3d2bedc4ccef3a7593
Turbobot <turbobot@vercel.com>
2025-07-24 00:38:58 +0530

    feat(create-turbo): apply official-starter transform

--- chat_app/frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Main from "@/components/Main";
-
-export default function Home() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <Main/>
-    </div>
-  )
-}
